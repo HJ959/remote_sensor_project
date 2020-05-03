453e8cd9a436a26f949c05671eca01571789d96f
HJ959 <henryjaames@gmail.com>
2020-05-03 12:23:10 +0100

    Arduino project

--- arduino_read_sensor/arduino_read_sensor.ino
+++ arduino_read_sensor/arduino_read_sensor.ino
@@ -1,3 +1,14 @@
+/*
+ * Created by Henry James
+ * https://henryjames.space
+ * 
+ * Programme to read sensor data and 
+ * print to serial for Pi to read, 
+ * part of remote sensor project.
+ */
+
+#define moisture A0
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -5,7 +16,7 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  int sensorValue = analogRead(A0);
+  int sensorValue = analogRead(moisture);
 
   Serial.println(sensorValue);
   delay(1000);
