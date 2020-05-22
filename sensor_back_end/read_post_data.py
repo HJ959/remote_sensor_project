8c001a65e2b079de5b888e44bf8fb7faa99ee9eb
HJ959 <henryjaames@gmail.com>
2020-05-22 16:02:06 +0100

    Python script reads the data and writes to a json

--- sensor_back_end/read_post_data.py
+++ sensor_back_end/read_post_data.py
@@ -1,12 +1,39 @@
+##############################################################################
 import serial
-ser = serial.Serial('/dev/ttyACM0', 9600)
+from time import sleep
+from datetime import date
+import re
+##############################################################################
+if __name__ == '__main__':
+    # b'Temp: 34, Moisture: 1023\r\n'
+    re_temp = re.compile(r'Temp: (.+?),') # group 1
+    re_moist = re.compile(r'Moisture: ([0-9]+)') # group 1
 
-while True: 
-    if(ser.in_waiting >0):
-        line = ser.readline()
-        line = str(line) + '/n'
-        break
+    ser = serial.Serial('/dev/ttyACM0', 9600)
+    
+    today = date.today()
 
-with open('sensor_data.txt', 'a') as f:
-    f.write(line)
+    for i in range(0, 100):
+        sleep(0.1)
+        if(ser.in_waiting >0):
+            line = str(ser.readline()) + '\n'
+   
+    # clean up the text line
+    temp = re.search(re_temp, line)
+    moist = re.search(re_moist, line)
+
+    if temp and moist:
+        temp = temp.group(1)
+        moist = moist.group(1)
+
+        output_json = '''
+"{today}" : {
+             "Celsius": "{temp}",
+             "Moisture_Level": "{moist}"
+            }
+'''
+
+    # write the data to a text file
+    with open('sensor_data.txt', 'a') as f:
+        f.write(output_json)
 
