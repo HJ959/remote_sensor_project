922799f6faba1d09dee619b1ddc59406e3c49b8a
HJ959 <henryjaames@gmail.com>
2020-05-22 16:32:58 +0100

    Need to get the json format in sensor_data.txt to stack correctly

--- sensor_back_end/read_post_data.py
+++ sensor_back_end/read_post_data.py
@@ -1,8 +1,9 @@
 ##############################################################################
 import serial
 from time import sleep
-from datetime import date
+from datetime import datetime
 import re
+import json
 ##############################################################################
 if __name__ == '__main__':
     # b'Temp: 34, Moisture: 1023\r\n'
@@ -11,27 +12,25 @@ if __name__ == '__main__':
 
     ser = serial.Serial('/dev/ttyACM0', 9600)
     
-    today = date.today()
+    now = datetime.now()
+    today = now.strftime('%d_%m_%Y_%H%M')
 
     for i in range(0, 100):
-        sleep(0.1)
+        sleep(0.01)
         if(ser.in_waiting >0):
-            line = str(ser.readline()) + '\n'
+            line = str(ser.readline())
    
     # clean up the text line
     temp = re.search(re_temp, line)
     moist = re.search(re_moist, line)
 
+    io = {}
     if temp and moist:
         temp = temp.group(1)
         moist = moist.group(1)
-
-        output_json = '''
-"{today}" : {
-             "Celsius": "{temp}",
-             "Moisture_Level": "{moist}"
-            }
-'''
+        
+        io[today] = {'Celsius': temp, 'Moisture_level': moist}
+        output_json = str(json.dumps(io, sort_keys=True, indent=4))
 
     # write the data to a text file
     with open('sensor_data.txt', 'a') as f:
