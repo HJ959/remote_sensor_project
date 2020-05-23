25f1a0ecba5c5550c35f6b1ce022688f1a996251
HJ959 <henryjaames@gmail.com>
2020-05-23 17:14:47 +0100

    Backend sensor upload - May_17-14-43_BST

---
+++ orchestrator.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# this shell script acts as the orchestrator
+# automating the various processes involved
+# 
+# Read sensor data
+# store sensor data
+# record audio and video
+# process audio and video
+# push to github for processing in front-end
+
+# path
+git_dir="/home/pi/remote_sensor_project"
+
+# check for repo updates (handy if wanting to edit script remotely)
+git --git-dir $git_dir/.git pull
+
+now="$(date | cut -d ' ' -f 3,4,5 | tr ' ' _ | tr ':' '-')"
+output_mp4="output_$now.mp4"
+output_png="out%d_$now.png"
+
+if [ ! -f "$git_dir/sensor_back_end/sensor_data.json" ]
+then
+	echo '{"example_json": {"temp": "25", "moisture": "670"}}' > $git_dir/sensor_back_end/sensor_data.json
+fi
+
+python3 $git_dir/sensor_back_end/read_post_data.py
+
+git --git-dir $git_dir/.git add --all
+git --git-dir $git_dir/.git commit -m "Backend sensor upload - $now"
+git --git-dir $git_dir/.git push -u origin master
+git --git-dir $git_dir/.git config --global credential.helper 'cache --timeout=3700'
