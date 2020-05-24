66caf480a1d9c1f1d7e5a448fabbb70f71de94b5
HJ959 <henryjaames@gmail.com>
2020-05-24 21:21:36 +0100

    Backend sensor upload - May_21-21-32_BST

--- sensor_back_end/orchestrator.sh
+++ sensor_back_end/orchestrator.sh
@@ -10,23 +10,23 @@
 # push to github for processing in front-end
 
 # path
-git_dir="/home/pi/remote_sensor_project"
+cd /home/pi/remote_sensor_project
 
 # check for repo updates (handy if wanting to edit script remotely)
-git --git-dir $git_dir/.git pull
+git pull
 
 now="$(date | cut -d ' ' -f 3,4,5 | tr ' ' _ | tr ':' '-')"
 output_mp4="output_$now.mp4"
 output_png="out%d_$now.png"
 
-if [ ! -f "$git_dir/sensor_back_end/sensor_data.json" ]
+if [ ! -f "sensor_back_end/sensor_data.json" ]
 then
-	echo '{"example_json": {"temp": "25", "moisture": "670"}}' > $git_dir/sensor_back_end/sensor_data.json
+	echo '{"example_json": {"temp": "25", "moisture": "670"}}' > sensor_back_end/sensor_data.json
 fi
 
-python3 $git_dir/sensor_back_end/read_post_data.py
+python3 sensor_back_end/read_post_data.py
 
-git --git-dir $git_dir/.git add $git_dir/sensor_back_end
-git --git-dir $git_dir/.git commit -m "Backend sensor upload - $now"
-git --git-dir $git_dir/.git push -u origin master
+git add --all
+git commit -m "Backend sensor upload - $now"
+git push -u origin master
 
